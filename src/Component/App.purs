3772739e1518086ae2e03c08ddf10d435b80d4f3
Christopher D. Parks <christopher.daniel.parks@gmail.com>
2015-10-06 15:54:12 -0700

    First version with working evaluation

--- src/Component/App.purs
+++ src/Component/App.purs
@@ -3,6 +3,12 @@ module Component.App where
 import Prelude
 
 import Data.Maybe
+import Data.Either
+import Data.Array (snoc)
+
+import Data.Syntax
+import Data.Parse
+import Data.Expr
 
 import qualified Thermite as T
 import qualified Thermite.Action as T
@@ -14,19 +20,18 @@ import qualified React.DOM.Props as RP
 import Component.Event
 
 type AppState =
-  { text         :: String
-  , errorMessage :: Maybe String
-  , scrutinee    :: Maybe String
-  , expressions  :: Array String
-  , definitions  :: Array String
+  { text    :: String
+  , expr    :: Maybe Expr
+  , history :: Array String
+  , error   :: Maybe String
   }
 
 data AppAction
   = DoNothing
   | NewText String
-  | CompleteText
-  | Evaluate
-  | Step
+  | ParseText
+  | Reduce Expr
+  | DismissAlert
 
 type AppProps = Unit
 
@@ -36,18 +41,44 @@ appClass = T.createClass (T.simpleSpec initialState update render)
   initialState :: AppState
   initialState =
     { text: ""
-    , errorMessage: Nothing
-    , scrutinee: Nothing
-    , expressions: []
-    , definitions: []
+    , expr: Nothing
+    , history: []
+    , error: Nothing
     }
 
   update :: T.PerformAction _ AppState _ AppAction
   update props action = T.modifyState (step action)
    where
-    step DoNothing      = \s -> s
-    step (NewText text) = \s -> s { text = text }
-    step CompleteText   = \s -> s { text = "", expressions = [], scrutinee = Just s.text }
+    step action =
+      case action of
+        DoNothing ->
+          \s -> s
+        NewText text ->
+          \s -> s { text = text, error = Nothing }
+        DismissAlert ->
+          \s -> s { error = Nothing }
+        ParseText ->
+          parse
+        Reduce expr ->
+          reduce expr
+
+  parse :: AppState -> AppState
+  parse s
+    | s.text == "" = s
+  parse s =
+    case parseAll parseSyntax s.text of
+      Left error   ->
+        s { text = "", error = Just (formatParseError error) }
+      Right syntax ->
+        s { text = "", history = [prettyPrint syntax], expr = Just (syntaxToExpr syntax) }
+
+  reduce :: Expr -> AppState -> AppState
+  reduce expr =
+    case step expr of
+      Nothing ->
+        \s -> s
+      Just expr' ->
+        \s -> s { history = s.history `snoc` prettyPrint (exprToSyntax expr'), expr = Just expr' }
 
   render :: T.Render _ AppState _ AppAction
   render send state props _ =
@@ -60,9 +91,43 @@ appClass = T.createClass (T.simpleSpec initialState update render)
         [ RP.className "row" ]
         [ input send state.text ]
       , RD.div
+        [ RP.className "row" ]
+        (alert send state.error)
+      , RD.div
         [ RP.className "row add-margin-large" ]
-        [ evaluator send state.expressions state.scrutinee ]
+        (evaluate send state.history state.expr)
       ]
+
+  alert :: _ -> Maybe String -> Array R.ReactElement
+  alert send Nothing = []
+  alert send (Just error) =
+    [ RD.div
+      [ RP.className "alert alert-danger"
+      , RP.onClick \_ -> send DismissAlert
+      ]
+      [ RD.text error ]
+    ]
+
+  evaluate :: _ -> Array String -> Maybe Expr -> Array R.ReactElement
+  evaluate send history Nothing = []
+  evaluate send history (Just expr) =
+    [ RD.div
+      [ RP.className "col-sm-12" ]
+      [ RD.div'
+        [ RD.button
+          [ RP.className "btn btn-default pull-right"
+          , RP.onClick \_ -> send (Reduce expr)
+          ]
+          [ RD.text "Step" ]
+        ]
+      , RD.div
+        [ RP.className "monospace-font" ]
+        (map renderSyntax history)
+      ]
+    ]
+
+  renderSyntax :: String -> R.ReactElement
+  renderSyntax syntax = RD.h4' [ RD.text syntax]
 
   header :: R.ReactElement
   header = RD.div
@@ -79,7 +144,7 @@ appClass = T.createClass (T.simpleSpec initialState update render)
     [ RD.div
       [ RP.className "input-group" ]
       [ RD.input
-        [ RP.className "form-control"
+        [ RP.className "form-control monospace-font"
         , RP.placeholder "<expression>"
         , RP.value value
         , RP.onKeyUp (handleKeyPress >>> send)
@@ -90,7 +155,7 @@ appClass = T.createClass (T.simpleSpec initialState update render)
         [ RP.className "input-group-btn" ]
         [ RD.button
           [ RP.className "btn btn-default"
-          , RP.onClick \_ -> send CompleteText
+          , RP.onClick \_ -> send ParseText
           ]
           [ RD.text "Parse" ]
         ]
@@ -114,7 +179,7 @@ handleKeyPress :: forall event. event -> AppAction
 handleKeyPress e =
   case getKeyCode e of
     13 ->
-      CompleteText
+      ParseText
     _  ->
       DoNothing
 
