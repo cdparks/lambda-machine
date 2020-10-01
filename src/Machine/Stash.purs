fa055810c16e0d9601c7fd8a2b9efff56afabdce
Christopher Parks <christopher.daniel.parks@gmail.com>
2020-09-30 19:14:33 -0700

    Add Stash representing suspended computations
    A Stash is a stack of Stacks. We currently only use this to evaluate the
    operand of a stuck application, but it can also be used to scrutinize
    structured data (which I may or may not add). This is also called the
    Dump in the literature. I just like that Stash looks like Stack (and we
    are stashing Stacks away for later).

---
+++ src/Machine/Stash.purs
@@ -0,0 +1,45 @@
+module Machine.Stash
+  ( Stash(..)
+  , empty
+  , suspend
+  , restore
+  ) where
+
+import Prelude
+
+import Control.Monad.State (class MonadState, gets, modify_)
+import Data.List (List(..), (:))
+import Data.Maybe (Maybe(..), maybe)
+import Data.Newtype (class Newtype)
+import Machine.Address (Address)
+import Machine.Stack (Stack, fromList)
+
+newtype Stash = Stash (List Stack)
+
+derive instance newtypeStash :: Newtype Stash _
+
+empty :: Stash
+empty = Stash Nil
+
+suspend :: forall s m. MonadState { stack :: Stack, stash :: Stash | s } m => m Unit
+suspend = do
+  {top, rest} <- gets _.stack
+  Stash stacks <- gets _.stash
+  modify_ $ _
+    { stack = {top, rest: Nil}
+    , stash = Stash $ extend stacks rest
+    }
+ where
+  extend stash = maybe stash (_ : stash) <<< fromList
+
+restore :: forall s m. MonadState { stack :: Stack, stash :: Stash | s } m => m (Maybe Address)
+restore = do
+  Stash stash <- gets _.stash
+  case stash of
+    Cons stack stacks -> do
+      modify_ $ _
+        { stack = stack
+        , stash = Stash stacks
+        }
+      pure $ Just stack.top
+    _ -> pure Nothing
