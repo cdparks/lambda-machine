72ed97c1d3eb95e7041f5d61bbda7ffd3a6b3e3c
Christopher Parks <christopher.daniel.parks@gmail.com>
2020-09-30 19:12:03 -0700

    Add Heap mapping addresses to nodes

---
+++ src/Machine/Heap.purs
@@ -0,0 +1,54 @@
+module Machine.Heap
+  ( Heap
+  , empty
+  , alloc
+  , fetch
+  , update
+  ) where
+
+import Prelude
+
+import Control.Monad.State (class MonadState, gets, modify_)
+import Data.HashMap (HashMap)
+import Data.HashMap as HashMap
+import Data.Maybe (Maybe(..))
+import Data.Newtype (wrap)
+import Data.Tuple (Tuple(..))
+import Machine.Address (Address)
+import Partial.Unsafe (unsafeCrashWith)
+
+type Heap a =
+  { memory :: HashMap Address a
+  , next :: Address
+  }
+
+empty :: forall a. Heap a
+empty = { memory: HashMap.empty, next: wrap 1 }
+
+alloc :: forall a s m. MonadState { heap :: Heap a | s } m => a -> m Address
+alloc node = modifyHeap \{ memory, next } -> Tuple next
+  { memory: HashMap.insert next node memory
+  , next: next + wrap 1
+  }
+
+fetch :: forall a s m. MonadState { heap :: Heap a | s } m => Address -> m a
+fetch address = withHeap \{ memory } ->
+  case HashMap.lookup address memory of
+    Just node -> node
+    Nothing -> unsafeCrashWith $ "Invalid address " <> show address
+
+update :: forall a s m. MonadState { heap :: Heap a | s } m => Address -> a -> m Unit
+update address node = modifyHeap \heap -> Tuple unit $ heap
+  { memory = HashMap.insert address node heap.memory
+  }
+
+modifyHeap :: forall a s m r. MonadState { heap :: Heap a | s } m => (Heap a -> Tuple r (Heap a)) -> m r
+modifyHeap f = do
+  heap0 <- gets _.heap
+  case f heap0 of
+    Tuple r heap1 -> do
+      modify_ $ _ { heap = heap1 }
+      pure r
+
+withHeap :: forall a s m r. MonadState { heap :: Heap a | s } m => (Heap a -> r) -> m r
+withHeap f = modifyHeap $ \heap -> Tuple (f heap) heap
