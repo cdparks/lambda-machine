395d91da437e9a45f0e91775d4e8dda2b4b533ad
Christopher Parks <christopher.daniel.parks@gmail.com>
2020-09-30 19:12:27 -0700

    Add Stack for keeping track of application nodes
    The Stack is always non-empty, which is why its representation is
    
      { top: Address, rest: List Address }
    
    Every element of rest must point to an application node. top is either
    another application node about to be unwound or another node type that
    is being applied.

---
+++ src/Machine/Stack.purs
@@ -0,0 +1,51 @@
+module Machine.Stack
+  ( Stack
+  , singleton
+  , push
+  , discard
+  , replace
+  , peek
+  , fromList
+  ) where
+
+import Prelude
+
+import Control.Monad.State (class MonadState, gets, modify_)
+import Data.List (List(..), index)
+import Data.Maybe (Maybe(..))
+import Machine.Address (Address)
+
+type Stack =
+  { top :: Address
+  , rest :: List Address
+  }
+
+singleton :: Address -> Stack
+singleton top = { top, rest: Nil }
+
+push :: forall s m. MonadState { stack :: Stack | s } m => Address -> m Unit
+push address = do
+  {top, rest} <- gets _.stack
+  modify_ $ _ { stack = {top: address, rest: Cons top rest} }
+
+discard :: forall s m. MonadState { stack :: Stack | s } m => m Unit
+discard = do
+  {rest} <- gets _.stack
+  case rest of
+    Cons y ys -> modify_ $ _ { stack = {top: y, rest: ys} }
+    Nil -> pure unit
+
+replace :: forall s m. MonadState { stack :: Stack | s } m => Address -> m Unit
+replace address = do
+  {rest} <- gets _.stack
+  modify_ $ _ { stack = {top: address, rest} }
+
+peek :: forall s m. MonadState { stack :: Stack | s } m => Int -> m (Maybe Address)
+peek i = do
+  {top, rest} <- gets _.stack
+  pure $ index (Cons top rest) i
+
+fromList :: List Address -> Maybe Stack
+fromList = case _ of
+  Nil -> Nothing
+  Cons top rest -> Just {top, rest}
