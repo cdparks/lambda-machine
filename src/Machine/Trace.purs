d7bda1e1d5799588eb2745126fe3a05609f048e2
Christopher Parks <christopher.daniel.parks@gmail.com>
2020-09-30 19:16:19 -0700

    Add Trace for describing state transitions

---
+++ src/Machine/Trace.purs
@@ -0,0 +1,28 @@
+module Machine.Trace
+  ( Trace(..)
+  ) where
+
+import Prelude
+
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
+import Language.Name (Name)
+import Machine.Address (Address)
+
+data Trace
+  = Start
+  | Unwound Address
+  | Followed Address
+  | Unfolded Name
+  | Fetched Name
+  | Instantiated Address
+  | Tied Name
+  | Substituted Name Address
+  | WentUnder Address
+  | Discarded Address Address
+  | Halted Address
+
+derive instance genericTrace :: Generic Trace _
+
+instance showTrace :: Show Trace where
+  show x = genericShow x
