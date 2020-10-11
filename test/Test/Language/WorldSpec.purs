bbb92d8af9ae9f605bb79beff1212d4c423ee60a
Christopher Parks <christopher.daniel.parks@gmail.com>
2020-10-11 14:23:31 -0700

    Add tests for World

---
+++ test/Test/Language/WorldSpec.purs
@@ -0,0 +1,101 @@
+module Test.Data.WorldSpec
+  ( spec
+  ) where
+
+import Test.Prelude
+
+import Language.World as World
+import Language.World (ConsistencyError(..), Dependency(..))
+import Data.Map as Map
+import Data.Set as Set
+
+spec :: Spec Unit
+spec = describe "Language.World" do
+  describe "World.define" do
+    it "disallows definitions that depend on non-extant names" do
+      let
+        world = World.new []
+        result = uncurry World.define (mkBind "x = y") world
+      result `shouldEqual` Left (Undefined $ Set.singleton y)
+
+    it "allows definitions that depend on extant names" do
+      let
+        world = World.new [mkBind "y = λa. a"]
+        result = uncurry World.define (mkBind "x = y") world
+      result `shouldEqual` Right
+        { nameToDeps: Map.fromFoldable
+          [ Tuple y $ Set.fromFoldable [Global x]
+          , Tuple x Set.empty
+          ]
+        , depToNames: Map.fromFoldable
+          [ Tuple (Global x) $ Set.fromFoldable [y]
+          , Tuple (Global y) Set.empty
+          ]
+        }
+
+  describe "World.undefine" do
+    it "disallows deleting definitions that have dependencies" do
+      let
+        world = World.new [mkBind "y a = a", mkBind "x = y"]
+        result = World.undefine y world
+      result `shouldEqual` Left (CannotDelete y $ Set.singleton $ Global x)
+
+    it "allows deleting definitions that have no dependencies" do
+      let
+        world = World.new [mkBind "y = λa. a", mkBind "x = y"]
+        result = World.undefine x world
+      result `shouldEqual` Right
+        { nameToDeps: Map.fromFoldable
+          [ Tuple y $ Set.empty
+          ]
+        , depToNames: Map.fromFoldable
+          [ Tuple (Global y) Set.empty
+          ]
+        }
+
+  describe "World.focus" do
+    it "disallows root expressions that depend on non-extant names" do
+      let
+        world = World.new []
+        result = World.focus (mkExpr "y") world
+      result `shouldEqual` Left (Undefined $ Set.singleton y)
+
+    it "allows root expressions that depend on extant names" do
+      let
+        world = World.new [mkBind "y = λa. a"]
+        result = World.focus (mkExpr "y") world
+      result `shouldEqual` Right
+        { nameToDeps: Map.fromFoldable
+          [ Tuple y $ Set.fromFoldable [Root]
+          ]
+        , depToNames: Map.fromFoldable
+          [ Tuple Root $ Set.fromFoldable [y]
+          , Tuple (Global y) Set.empty
+          ]
+        }
+
+  describe "World.unfocus" do
+    it "removes root as a dependency" do
+      let
+        world = World.focus (mkExpr "y") $ World.new [mkBind "y = λa. a"]
+        result = World.unfocus <$> world
+      result `shouldEqual` Right
+        { nameToDeps: Map.fromFoldable
+          [ Tuple y $ Set.empty
+          ]
+        , depToNames: Map.fromFoldable
+          [ Tuple (Global y) Set.empty
+          ]
+        }
+
+    it "has no effect if world has no focus" do
+      let
+        world =  World.new [mkBind "y = λa. a"]
+        result = World.unfocus world
+      result `shouldEqual` world
+
+x :: Name
+x = name_ "x"
+
+y :: Name
+y = name_ "y"
