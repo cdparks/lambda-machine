dc52d97ff9003f6f3d2366bbcae2e0ae058b62b6
Christopher Parks <christopher.daniel.parks@gmail.com>
2021-04-26 22:50:38 -0700

    Truncate history to 20 expressions when rendering
    This avoids some nasty slowdown for long running sessions

--- src/Components/Expressions.purs
+++ src/Components/Expressions.purs
@@ -4,24 +4,34 @@ module Components.Expressions
 
 import Lambda.Prelude
 
-import Lambda.Language.PrettyPrint (Rep, Doc, selectRep)
+import Data.Array as Array
+import Lambda.Language.PrettyPrint (Rep, Doc, withRep)
 import React.Basic (JSX)
 import React.Basic.DOM as R
 
 type Props =
-  { history :: Array (Doc String)
+  { history :: List (Doc String)
   , rep :: Rep
   }
 
 component :: Props -> JSX
-component {history, rep} =
-  R.ul
-    { className: "unstyled scroll-overflow"
-    , children: map renderExpr history
-    }
+component {history, rep} = R.ul
+  { className: "unstyled scroll-overflow"
+  , children: truncate rep history
+  }
+
+truncate :: Rep -> List (Doc String) -> Array JSX
+truncate rep = Array.fromFoldable <<< loop 20
  where
-  renderExpr expr =
-    R.li
-      { className: "expression"
-      , children: [R.text $ selectRep expr rep]
-      }
+  loop :: Int -> List (Doc String) -> List JSX
+  loop n = case _ of
+    Nil -> Nil
+    Cons t ts
+      | n <= 0 -> item "…" : Nil
+      | otherwise -> item (withRep rep t) : loop (n - 1) ts
+
+  item :: String -> JSX
+  item t = R.li
+    { className: "expression"
+    , children: [R.text t]
+    }
