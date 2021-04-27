2d63b4bb935c3a207d64393ef4771981dae4504c
Christopher Parks <christopher.daniel.parks@gmail.com>
2021-04-26 22:46:06 -0700

    Add guard, trace, traceM, spy to Lambfda.Prelude

--- src/Lambda/Prelude.purs
+++ src/Lambda/Prelude.purs
@@ -8,6 +8,7 @@ import Prelude
 import Control.Alt ((<|>)) as X
 import Control.Monad.Rec.Class (class MonadRec, Step(..), tailRecM) as X
 import Control.Monad.State (class MonadState, evalState, execState, gets, get, modify_, put) as X
+import Control.MonadZero (guard) as X
 import Data.Either (Either(..), either) as X
 import Data.Foldable (class Foldable, fold, foldr, foldl, foldM, foldMap, for_, traverse_) as X
 import Data.Generic.Rep (class Generic) as X
@@ -26,4 +27,5 @@ import Data.Traversable (class Traversable, traverse, for) as X
 import Data.Tuple (Tuple(..), uncurry, fst, snd) as X
 import Data.Tuple.Nested ((/\)) as X
 import Data.Unfoldable (class Unfoldable) as X
+import Debug.Trace (trace, traceM, spy) as X
 import Effect (Effect) as X
