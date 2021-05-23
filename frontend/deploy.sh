398b615509be1ca33865f3c97242088dbbb75d3f
Christopher Parks <christopher.daniel.parks@gmail.com>
2021-05-23 13:24:34 -0700

    Update deploy script

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -10,16 +10,24 @@ if [ "$branch" != "main" ]; then
 fi
 
 if [ ! -d "./dist" ]; then
-  echo "dist directory doesn't exist; run yarn deploy or yarn bundle"
+  echo "./dist directory doesn't exist; run yarn deploy or yarn bundle"
   exit 2
 fi
+
+git_root=$(git rev-parse --show-toplevel)
+pushd git_root > /dev/null
+trap "popd > /dev/null" EXIT
+
+now=$(date)
 
 git branch -D gh-pages 2>/dev/null || true
 git branch -D draft 2>/dev/null || true
 git checkout -b draft
-cp ./static/CNAME ./dist/CNAME
+mv ./frontend/dist ./dist
+cp ./frontend/static/CNAME ./dist/CNAME
+echo "$now" > ./dist/deployed.txt
 git add -f ./dist
-git commit -am "Deploy to gh-pages on $(date)"
+git commit -am "Deploy to gh-pages on $now"
 git subtree split --prefix dist -b gh-pages
 git push --force origin gh-pages:gh-pages
 git checkout main
