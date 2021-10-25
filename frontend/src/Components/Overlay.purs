c86e5f2f3c76bba40604d37a66cfc7701ca3af34
Christopher Parks <christopher.daniel.parks@gmail.com>
2021-10-24 23:02:22 -0700

    Add Overlay component as a base for modals and spinners

---
+++ frontend/src/Components/Overlay.purs
@@ -0,0 +1,36 @@
+module Components.Overlay
+  ( new
+  ) where
+
+import Lambda.Prelude
+
+import React.Basic (JSX)
+import React.Basic.DOM as R
+import React.Basic.Hooks (Component, component)
+import React.Portal as Portal
+import React.Basic.Events (handler)
+import React.Basic.DOM.Events (stopPropagation)
+
+type Props =
+  { dismiss :: Maybe (Effect Unit)
+  , children :: Array JSX
+  }
+
+new :: Component Props
+new = do
+  portal <- Portal.new
+  component "Overlay" \{ dismiss, children } ->
+    pure $ portal
+      { children:
+        [ R.div
+          { className: "overlay"
+          , children:
+            [ R.div
+              { className: "overlay-item"
+              , children
+              }
+            ]
+          , onClick: handler stopPropagation $ const $ sequence_ dismiss
+          }
+        ]
+      }
