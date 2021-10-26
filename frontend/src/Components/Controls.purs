c03a052a6bb37551ec2b00728b5105d10d2f127c
Christopher Parks <christopher.daniel.parks@gmail.com>
2021-10-25 21:19:28 -0700

    Disable buttons in Control component when the onClick is Nothing

--- frontend/src/Components/Controls.purs
+++ frontend/src/Components/Controls.purs
@@ -5,92 +5,70 @@ module Components.Controls
 import Lambda.Prelude
 
 import Lambda.Language.Pretty (Rep, select)
-import Lambda.Machine (Machine)
 import React.Basic (JSX)
 import React.Basic.DOM as R
-import React.Basic.Events (EventHandler, handler_)
+import React.Basic.Events (handler_)
 
 type Props =
-  { machine :: Maybe Machine
-  , onStep :: Machine -> Effect Unit
-  , onClear :: Effect Unit
-  , onShare :: Effect Unit
-  , onSave :: Effect Unit
-  , onSugar :: Effect Unit
+  { onStep :: Maybe (Effect Unit)
+  , onClear :: Maybe (Effect Unit)
+  , onShare :: Maybe (Effect Unit)
+  , onSave :: Maybe (Effect Unit)
+  , onSugar :: Maybe (Effect Unit)
   , rep :: Rep
   }
 
 component :: Props -> JSX
-component {machine, onStep, onClear, onShare, onSave, onSugar, rep} =
+component {onStep, onClear, onShare, onSave, onSugar, rep} =
   R.div
     { className: "add-margin-medium btn-group pull-right"
     , children:
-      [ button (running =<< machine)
-        { className: "btn btn-default"
+      [ button
+        { label: "Step"
+        , className: "btn btn-default"
         , onClick: onStep
-        , label: "Step"
         }
-      , button machine
-        { className: "btn btn-default"
-        , onClick: const onClear
-        , label: "Clear"
+      , button
+        { label: "Clear"
+        , className: "btn btn-default"
+        , onClick: onClear
         }
-      , button enabled
-        { className: "btn btn-default"
-        , onClick: const onShare
-        , label: "Share"
+      , button
+        { label: "Share"
+        , className: "btn btn-default"
+        , onClick: onShare
         }
-      , button enabled
-        { className: "btn btn-default"
-        , onClick: const onSave
-        , label: "Save"
+      , button
+        { label: "Save"
+        , className: "btn btn-default"
+        , onClick: onSave
         }
-      , button enabled
-        { className: select rep
+      , button
+        { label: select rep
+          { sugar: "Raw"
+          , raw: "Sugar"
+          }
+        , className: select rep
           { sugar: "btn btn-danger"
           , raw: "btn btn-success"
           }
-        , onClick: const onSugar
-        , label: select rep
-            { sugar: "Raw"
-            , raw: "Sugar"
-            }
+        , onClick: onSugar
         }
       ]
     }
- where
-  enabled = pure unit
 
--- | Create a button element enabled based on the presence of the first
--- | argument.
+-- | Disable button if `onClick` is `Nothing`
 button
-  :: forall a
-   . Maybe a
-  ->
-    { className :: String
-    , onClick :: a -> Effect Unit
-    , label :: String
+  ::
+    { label :: String
+    , className :: String
+    , onClick :: Maybe (Effect Unit)
     }
   -> JSX
-button m {className, onClick, label} =
+button { label, className, onClick } =
   R.button
-    { className: maybeEnable className m
-    , onClick: maybeHandle onClick m
+    { className
+    , onClick: handler_ $ fromMaybe (pure unit) $ onClick
     , children: [R.text label]
+    , disabled: isNothing onClick
     }
-
--- | Build handler if second argument is present.
-maybeHandle :: forall a. (a -> Effect Unit) -> Maybe a -> EventHandler
-maybeHandle handle =
-  handler_ <<< maybe (pure unit) handle
-
--- | Enable element if second argument is present.
-maybeEnable :: forall a. String -> Maybe a -> String
-maybeEnable className =
-  maybe
-    (className <> " disabled")
-    (const className)
-
--- | A machine that hasn't halted
-running :: Machine -> Maybe Machine
-running machine = machine <$ guard (not $ machine.halted)
